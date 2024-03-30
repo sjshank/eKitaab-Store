b672f782fead3046cbe5fae3049036cd8a6ef59f
Saurabh Shankariya <sshankariya@gmail.com>
2024-03-30 20:41:47 +0530

    fix: sitemap

--- src/utils/generate-sitemap.mjs
+++ src/utils/generate-sitemap.mjs
@@ -1,23 +1,23 @@
-import fs from 'fs';
-import { globby } from 'globby';
+import fs from "fs";
+import { globby } from "globby";
 
 function addPage(page) {
   const path = page
-    .replace('pages', '')
-    .replace('src/', '')
-    .replace('.tsx', '')
-    .replace('.mdx', '');
-  const route = path === '/index' ? '' : path;
+    .replace("pages", "")
+    .replace("src/", "")
+    .replace(".tsx", "")
+    .replace(".mdx", "");
+  const route = path === "/index" ? "" : path;
 
   const URL =
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000'
-      : 'https://ekitaab-store.netlify.app';
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://ekitaab-store.vercel.app";
 
   return `  <url>
     <loc>${`${URL}${route}`}</loc>
     <lastmod>${`${new Date()}`}</lastmod>
-    <changefreq>daily</changefreq>
+    <changefreq>monthly</changefreq>
   </url>`;
 }
 
@@ -26,18 +26,18 @@ async function generateSitemap() {
   //Ignore 404, 500, error files
   //Consider only app route
   const pages = await globby([
-    'src/pages/**/*{.tsx,.mdx}',
-    '!src/pages/_*.tsx',
-    '!src/pages/api',
-    '!src/pages/404.tsx',
-    '!src/pages/500.tsx'
+    "src/pages/**/*{.tsx,.mdx}",
+    "!src/pages/_*.tsx",
+    "!src/pages/api",
+    "!src/pages/404.tsx",
+    "!src/pages/500.tsx",
   ]);
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${pages.map(addPage).join('\n')}
+${pages.map(addPage).join("\n")}
 </urlset>`;
 
-  fs.writeFileSync('public/sitemap.xml', sitemap);
+  fs.writeFileSync("public/sitemap.xml", sitemap);
 }
 
 generateSitemap();
