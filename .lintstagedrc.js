b57dfdea15026ece386c50b684d23f1a87a19fc5
Saurabh Shankariya <sshankariya@gmail.com>
2024-04-05 13:29:14 +0530

    test: eslint ignore pattern update

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   // Type check TypeScript files
-  '**/*.(ts|tsx)': () => 'npm run tsc --noEmit',
+  "**/*.(ts|tsx)": () => "npm run tsc --noEmit",
 
   // Lint & Prettify TS and JS files
-  '**/*.(ts|tsx|js)': (filenames) => [
-    `npm run lint ${filenames.join(' ')}`,
-    `npm run prettier --write ${filenames.join(' ')}`
+  "**/*.(ts|tsx|js)": (filenames) => [
+    `npm run lint ${filenames.join(" ")}`,
+    `npm run prettier --write ${filenames.join(" ")}`,
   ],
 
   // Prettify only Markdown and JSON files
-  '**/*.(md|json)': (filenames) =>
-    `npm run prettier --write ${filenames.join(' ')}`
+  "**/*.(md|json)": (filenames) =>
+    `npm run prettier --write ${filenames.join(" ")}`,
 };
